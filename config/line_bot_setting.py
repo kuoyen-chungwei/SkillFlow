cacf7a9f31fb45da9e4afc45b3ef6ec721e23df6
kuoyen-chungwei <kuoyen.chungwei@gmail.com>
2025-09-30 22:35:14 +0800

    Update line_bot_setting.py

--- config/line_bot_setting.py
+++ config/line_bot_setting.py
@@ -1,4 +1,5 @@
 CHANNEL_ACCESS_TOKEN = 'ry9jEoZkaan4jXdAVK1KaLgS7BkdiUEDWRhT2NeU09+lzWpW1lbBQ/xhVEskXSNiTdYtF1mDEjwP4RQhn/v/pH3lFoVgwHo5BIoTd3wnJqZptvJWx5SeKs7NCVoZ+qYTZwz2NzBPUvwaSOKFePcM6QdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET = '940f0961ea6aee2849c743612ce8f7f2'
-BASE_URL = 'https://b3e361362a36.ngrok-free.app'
+BASE_URL = 'https://chungwei-peichen.zeabur.app'
 # https://skill-flow-seven.vercel.app/
+
